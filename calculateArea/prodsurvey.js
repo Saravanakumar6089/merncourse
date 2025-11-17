68b5673593fbd8946c4100b052115b045e5ed24e
Saravanakumar6089 <skvel6089@gmail.com>
2025-11-17 08:30:24 -0500

    event handling in javascript

---
+++ calculateArea/prodsurvey.js
@@ -0,0 +1,28 @@
+
+function submitFeedback() {
+    alert("thank you for your feedback");
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+
+
+        document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+document.getElementById('submitBtn').addEventListener('click',submitFeedback);
+document.addEventListener('keypress', function(event){
+    if(event.key=='Enter'){
+        submitFeedback();
+    }
+})
