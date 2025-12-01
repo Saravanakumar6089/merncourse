af94c8ebc8d985fe3fa6b44a8fff18155350beea
Saravanakumar6089 <skvel6089@gmail.com>
2025-12-01 07:36:47 -0500

    writing game logic

--- color_memory_game/color_memory.js
+++ color_memory_game/color_memory.js
@@ -3,7 +3,8 @@ const democt = document.getElementById('demo-content');
 const gamegd = document.getElementById('game-grid');
 
 //to get the option from user
-let rows = 0, cols = 0;
+let diff=0; //default - easy - 0; 
+/* medium - 1; hard - 2; super hard - 3 */
 const options = document.querySelectorAll('.diff-option');
 
 //add event listener for each option to call with 'gridsize' param
@@ -13,16 +14,11 @@ options.forEach((op)=>{
 })
 
 function setDifficultyAndStart(gridsize){
-    if(gridsize==5){
-        rows = 2; cols = 5;
-    }else{
-        rows = gridsize; cols = gridsize;
-    }
     stmenu.classList.add('hidden');
     democt.classList.add('hidden');
     gamegd.classList.remove('hidden');
 
-    startGame(rows, cols);//yet to be written
+    startGame(gridsize);//YTC
 }
 
 //set demo content
@@ -30,3 +26,11 @@ document.getElementById('how-to-play-btn').addEventListener('click', () => {
     democt.classList.remove('hidden');
     democt.innerHTML = "<p>Match all pairs of colors before the time runs out! Click a card to flip it.</p>";
 });
+
+//game logic
+function startGame(d){
+    cardLayout.innerHTML = '';
+    cardLayout.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    const totalCards = size * size;
+    //YTC
+}
